0bd0d675d7fada0b7ea17fceabd0a3940b4af6d1
Kevin Turner <kevin@ksturner.com>
2022-03-30 21:20:59 -0500

    adding in express server

--- front/Button.tsx
+++ front/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button({ children }) {
     return (
-        <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+        <button className="text-white m-2 p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
             {children}
         </button>
     );
